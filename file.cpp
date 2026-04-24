0195776e5988d353fa9f1ea37e03f80435674f0c
NainaKalra <NKalra01@indianatech.net>
2026-04-23 23:27:54 -0400

    added find,delete and display functions in linked list

--- file.cpp
+++ file.cpp
@@ -118,7 +118,7 @@ struct LinkedList{
         size = 0;
     }
 
-//add patient to linked list
+//adding patient to linked list
 void addPatient(Patient p){
     Node* newNode = new Node(p);
 
@@ -134,6 +134,98 @@ void addPatient(Patient p){
     }
     size++;
     cout<<"Patient "<<p.name<<" added with ID "<<p.id<<endl;
+}
+
+//finding patient by id in linked list
+Node* findPatient(int id)
+{
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        if (temp->data.id == id)
+            return temp;
+        temp = temp->next;
+    }
+    return nullptr;
+}
+
+//removing patient from linked list
+void removePatient(int id)
+{
+    if (head == nullptr)
+    {
+        cout << "No patients found." << endl;
+        return;
+    }
+
+    if (head->data.id == id)
+    {
+        Node *Delete = head;
+        head = head->next;
+        delete Delete;
+        size--;
+        cout << "Patient removed." << endl;
+        return;
+    }
+
+    Node *temp = head;
+    while (temp->next != nullptr)
+    {
+        if (temp->next->data.id == id)
+        {
+            Node *Delete = temp->next;
+            temp->next = Delete->next;
+            delete Delete;
+            size--;
+            cout << "Patient removed." << endl;
+            return;
+        }
+        temp = temp->next;
+    }
+    cout << "Patient not found." << endl;
+}
+
+//displaying all patients in linked list
+void display()
+{
+    if (head == nullptr)
+    {
+        cout << "No patients registered." << endl;
+        return;
+    }
+
+    cout << "\n-----All Patients------" << endl;
+    Node *temp = head;
+    int num = 1;
+
+    while (temp != nullptr)
+    {
+        string sev = "";
+        if (temp->data.severity == 1)
+            sev = "Mild";
+        else if (temp->data.severity == 2)
+            sev = "Moderate";
+        else if (temp->data.severity == 3)
+            sev = "Severe";
+        else
+            sev = "Critical";
+
+        cout << num << ". ID: " << temp->data.id
+             << " Name: " << temp->data.name
+             << " Age: " << temp->data.age
+             << " City: " << temp->data.location
+             << " Severity: " << sev
+             << " Status: " << temp->data.status
+             << " Tested: " << (temp->data.tested ? "Yes" : "No");
+
+        if (temp->data.tested)
+            cout << " Result: " << (temp->data.isPositive ? "Positive" : "Negative");
+
+        cout << endl;
+        temp = temp->next;
+        num++;
+    }
+    cout << "Total: " << size << " patients" << endl;
 }
 
 // counting the number of nodes in linkedlist
